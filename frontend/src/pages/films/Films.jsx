17b832554cceea8aac670282434455d16b509e04
obpalomo <obpalomo@gmail.com>
2024-02-03 08:32:51 +0100

    forms

---
+++ frontend/src/pages/films/Films.jsx
@@ -0,0 +1,7 @@
+export default function Films(){
+    return(
+        <>
+            <h2>Películas</h2>
+        </>
+    )
+}
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "../../components/loginForm/LoginForm"
+
+export default function Login(){
+    return(
+        <>
+            <h2>Acceso</h2>
+            <LoginForm></LoginForm>
+        </>
+    )
+}
